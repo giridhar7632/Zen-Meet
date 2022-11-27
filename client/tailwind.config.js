9f778763734f678ba28448ea71ad0acdeb54a756
Talla Giridhar <giridhar.talla2002@gmail.com>
2022-11-27 11:14:44 +0530

    update style config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        laptop: '1024px',
+        desktop: '1281px',
+      },
+      fontFamily: {
+        sans: [
+          'Inter',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica Neue',
+          'Ubuntu',
+          'sans-serif',
+        ],
+      },
+    },
   },
-  plugins: [],
 }
