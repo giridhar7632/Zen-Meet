8d44fc1644f3ec0361dfbb620b30e559ecd722f1
Talla Giridhar <giridhar.talla2002@gmail.com>
2022-11-27 10:54:29 +0530

    add chats

--- src/app.js
+++ src/app.js
@@ -5,11 +5,18 @@ const { connect } = require('mongoose')
 const { URI } = require('./utils/config')
 const indexRouter = require('./routes/index')
 const authRouter = require('./routes/auth')
+const chatsRouter = require('./routes/chats')
 const logger = require('./utils/logger')
 const { requestLogger } = require('./utils/middleware')
 
 const app = express()
 
+app.use((_req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next()
+})
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
@@ -17,6 +24,7 @@ app.use(requestLogger)
 
 app.use('/', indexRouter)
 app.use('/auth', authRouter)
+app.use('/chats', chatsRouter)
 
 connect(URI, {
   useNewUrlParser: true,
